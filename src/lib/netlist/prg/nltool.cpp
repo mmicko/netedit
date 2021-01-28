815723fc4772ee7f47558d1a63abc189a65e01b6
Stiletto <stilett0@users.noreply.github.com>
2021-01-27 23:16:46 -0500

    Update 2020 to 2021.
    Update 2020 to 2021.

--- src/lib/netlist/prg/nltool.cpp
+++ src/lib/netlist/prg/nltool.cpp
@@ -425,7 +425,7 @@ int main(int argc, char *argv[])
 	{
 		pout(
 			"nltool (netlist) 0.1\n"
-			"Copyright (C) 2020 Couriersud\n"
+			"Copyright (C) 2021 Couriersud\n"
 			"License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>.\n"
 			"This is free software: you are free to change and redistribute it.\n"
 			"There is NO WARRANTY, to the extent permitted by law.\n\n"
